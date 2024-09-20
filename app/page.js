00c39f4ee1fc656b9a8fb597bd5f2a88de13f209
Zayn Yusuf <yusue786@gmail.com>
2024-09-20 23:27:54 +0600

    adding site url

--- app/page.js
+++ app/page.js
@@ -1,23 +1,5 @@
 "use client";
 
-import { useState } from "react";
-import LoginForm from "./components/LoginForm";
-
 export default function Home() {
-  const [image, setImage] = useState(false);
-  return (
-    <>
-      {!image ? (
-        <LoginForm setImage={setImage} />
-      ) : (
-        <div className="w-[60%] mx-auto">
-          <img
-            src="/images/banner.png"
-            alt="megaeprsonals"
-            className="w-full h-full object-cover"
-          />
-        </div>
-      )}
-    </>
-  );
+  return <div>Escortaligaterlistcrawler</div>;
 }
